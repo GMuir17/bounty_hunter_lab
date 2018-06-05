e7a3712a866bca4dc56623137d556e12ab17b22a
Gary Muir <garymuir17@gmail.com>
2018-06-05 19:33:09 +0100

    delete and update methods added

--- models/bounty.rb
+++ models/bounty.rb
@@ -32,6 +32,38 @@ class Bounty
     db.close()
   end
 
+  def delete()
+    db = PG.connect({
+      dbname: "bounty_list",
+      host: "localhost"
+      })
+    sql = "DELETE FROM bounties WHERE id = $1;"
+    values = [@id]
+    db.prepare("delete", sql)
+    db.exec_prepared("delete", values)
+    db.close()
+  end
+
+  def update()
+    db = PG.connect({
+      dbname: "bounty_list",
+      host: "localhost"
+      })
+    sql = "UPDATE bounties
+      SET (name,
+           species,
+           last_known_location,
+           bounty_value)
+      = ($1, $2, $3, $4) WHERE id = $5;"
+    values = [@name, @species, @last_known_location, @bounty_value, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close()
+  end
+
+
+
+
 
   def Bounty.all()
     db = PG.connect({
